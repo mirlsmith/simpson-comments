e105ff94d20a5758e942e65053062bb214b29d36
Miriam Smith <miriamzeff@gmail.com>
2023-01-24 23:06:49 +0200

    cleanup

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,36 +1,33 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { COMMENTS } from '../data/comment';
-import { Comment } from '../models/comment';
-import { UtilService } from './util.service';
+import { Injectable } from '@angular/core'
+import { BehaviorSubject, Observable, of } from 'rxjs'
+import { COMMENTS } from '../data/comment'
+import { Comment } from '../models/comment'
+import { UtilService } from './util.service'
 
 const STORAGE_KEY = 'commentsDB'
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
-
-  constructor(
-    private utilService:UtilService
-  ) { }
+  constructor(private utilService: UtilService) {}
 
   private _comments$ = new BehaviorSubject<Comment[]>([])
   public comments$ = this._comments$.asObservable()
 
   private commentsDB: Comment[] = this.loadComments()
 
-  query(){
-    const comments = this.commentsDB.filter(comment => !comment.parentCommentId).sort(
-      (a : Comment, b : Comment) =>  {
+  query() {
+    const comments = this.commentsDB
+      .filter((comment) => !comment.parentCommentId)
+      .sort((a: Comment, b: Comment) => {
         const dateA = new Date(a.createdAt).getTime()
         const dateB = new Date(b.createdAt).getTime()
         return dateA - dateB
-      }
-    )
+      })
     this._comments$.next(comments)
   }
 
-  save(comment: Comment): Observable<Comment>{
+  save(comment: Comment): Observable<Comment> {
     comment.createdAt = Date()
     return comment.id ? this._edit(comment) : this._add(comment)
   }
@@ -45,7 +42,7 @@ export class CommentService {
 
   private _edit(comment: Comment) {
     const comments = this.commentsDB
-    const commentIdx = comments.findIndex(cmnt => cmnt.id === comment.id)
+    const commentIdx = comments.findIndex((cmnt) => cmnt.id === comment.id)
     comments.splice(commentIdx, 1, comment)
     this._comments$.next(comments)
     this.query()
@@ -53,49 +50,51 @@ export class CommentService {
     return of(comment)
   }
 
-
   loadComments() {
     let comments = this.utilService.loadFromStorage(STORAGE_KEY)
     if (!comments) {
       comments = COMMENTS
       this.utilService.saveToStorage(STORAGE_KEY, comments)
     }
-    return comments    
+    return comments
   }
 
-  getReplies(commentId: number){
-    return this.commentsDB.filter(comment => comment.parentCommentId === commentId)
+  getReplies(commentId: number) {
+    return this.commentsDB.filter(
+      (comment) => comment.parentCommentId === commentId
+    )
   }
 
-  getById(commentId: number) : Observable<Comment> {
-    const comment = this.commentsDB.find(comment => comment.id === commentId)
-    return comment ? of({...comment}) : of()
+  getById(commentId: number): Observable<Comment> {
+    const comment = this.commentsDB.find((comment) => comment.id === commentId)
+    return comment ? of({ ...comment }) : of()
   }
 
   remove(commentId: number) {
-    console.log('removing comment', commentId);
+    console.log('removing comment', commentId)
     const comments = this.commentsDB
     const replies = this.getReplies(commentId)
-    if (replies.length) replies.forEach( 
-      cmnt => {
+    if (replies.length)
+      replies.forEach((cmnt) => {
         if (cmnt.deletedAt) return
         this.remove(cmnt.id)
       })
-    const commentIdx = comments.findIndex(comment => comment.id === commentId)
+    const commentIdx = comments.findIndex(
+      (comment) => comment.id === commentId
+    )
     comments[commentIdx].deletedAt = new Date()
     this.query()
     this.utilService.saveToStorage(STORAGE_KEY, comments)
   }
 
-
   getEmptyComment(): Comment {
     return {
       id: 0,
-    parentCommentId: null,
-    ownerId: 0,
-    txt: '',
-    createdAt: '',
-    deletedAt: null
+      parentCommentId: null,
+      ownerId: 0,
+      txt: '',
+      createdAt: '',
+      deletedAt: null,
     }
   }
 }
