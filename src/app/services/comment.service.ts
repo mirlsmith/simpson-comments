1c57909a320201b437b05fd60d4c51d4b599278c
Miriam Smith <miriamzeff@gmail.com>
2023-01-24 15:49:05 +0200

    comment service functions

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { COMMENTS } from '../data/comment';
+import { Comment } from '../models/comment';
+import { UtilService } from './util.service';
 
+const STORAGE_KEY = 'commentsDB'
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(
+    private utilService:UtilService
+  ) { }
+
+  private _comments$ = new BehaviorSubject<Comment[]>([])
+  public comments$ = this._comments$.asObservable()
+
+  private commentsDB: Comment[] = this.loadComments()
+
+
+  query(){
+    this._comments$.next(this.commentsDB)
+  }
+
+  loadComments() {
+    let comments = this.utilService.loadFromStorage(STORAGE_KEY)
+    if (!comments) {
+      comments = COMMENTS
+      this.utilService.saveToStorage(STORAGE_KEY, comments)
+    }
+    return comments    
+  }
 }
