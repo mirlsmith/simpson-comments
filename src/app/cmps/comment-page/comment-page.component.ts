d4fe7ef11f71bbce7b3f063e26cd933824298c6c
Miriam Smith <miriamzeff@gmail.com>
2023-01-25 15:12:45 +0200

    add response comment temp fix

--- src/app/cmps/comment-page/comment-page.component.ts
+++ src/app/cmps/comment-page/comment-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Comment } from 'src/app/models/comment';
 import { CommentService } from 'src/app/services/comment.service';
 
@@ -8,20 +8,31 @@ import { CommentService } from 'src/app/services/comment.service';
   templateUrl: './comment-page.component.html',
   styleUrls: ['./comment-page.component.scss'],
 })
-export class CommentPageComponent implements OnInit {
+export class CommentPageComponent implements OnInit, OnDestroy {
   constructor(private commentService: CommentService) {}
 
   comments!: Comment[];
   comments$!: Observable<Comment[]>;
 
+  subscription!: Subscription
+
   ngOnInit(): void {
     this.commentService.query();
     this.comments$ = this.commentService.comments$;
+
+    this.subscription = this.commentService.comments$.subscribe(
+      comnts => this.comments = comnts
+    )
   }
 
   onRemoveComment(commentId: number): void {
     this.commentService.remove(commentId)
   }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe()
+  }
+
 
 
 }
